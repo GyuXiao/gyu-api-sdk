8ebb7abfac27bc0b8a0f6ca89e0b68a8f6a199ae
GyuXiao <2075276808@qq.com>
2024-06-04 17:37:00 +0800

    feat: 1, 规范化通用化 client 的使用

--- examples/user.go
+++ examples/user.go
@@ -2,39 +2,43 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/GyuXiao/gyu-api-sdk/client"
 	"github.com/GyuXiao/gyu-api-sdk/sdk"
 	"github.com/GyuXiao/gyu-api-sdk/sdk/request"
 	"github.com/GyuXiao/gyu-api-sdk/sdk/response"
 	"github.com/GyuXiao/gyu-api-sdk/service/user"
 )
 
-var accessKey string = "gyu"
-var secretKey string = "abcdefg"
-
 // 用户需要做的，只是配置好一个 config，然后向目标 url 发起请求即可
 
 func main() {
-	// 使用 config 里的配置创建 client
-	config := sdk.NewConfig(accessKey, secretKey)
-	client, err := user.NewClient(config)
+	// 使用 SDK 分 3 步走
+	// 下面以向 user 服务发起请求作为例子
+
+	// 1,使用 config 里的配置创建 client
+	config := sdk.NewConfig(user.AccessKey, user.SecretKey)
+	clt, err := client.NewClient(config)
 	if err != nil {
-		client.Logger.Errorf("客户端创建错误: %v", err)
+		clt.Logger.Errorf("客户端创建错误: %v", err)
 		panic(err)
 	}
-	// 创建一个 user 对象
-	userTest := user.NewUser("user_test1")
+
+	// 具体情况具体分析，比如这里的 request body 是一个 user 对象
+	userTest := user.NewUser(user.TestUsername)
 	userJson, _ := json.Marshal(userTest)
-	// 创建请求
+
+	// 2,构建服务请求
 	req := &request.BaseRequest{
-		URL:    "http://127.0.0.1:8123/api/user",
-		Method: "POST",
+		URL:    user.ServiceUrl,
+		Method: user.MethodPost,
 		Header: nil,
 		Body:   string(userJson),
 	}
-	// 通过 client 发起请求
 	resp := &response.BaseResponse{}
-	err = client.Send(req, resp)
+
+	// 3,通过 client 向 user 服务发起一个 POST 请求
+	err = clt.Send(req, resp)
 	if err != nil {
-		client.Logger.Errorf("客户端请求错误: %v", err)
+		clt.Logger.Errorf("客户端请求错误: %v", err)
 	}
 }
--- service/user/user.go
+++ service/user/user.go
@@ -4,6 +4,13 @@ type User struct {
 	Username string `json:"username"`
 }
 
+const ServiceUrl = "http://127.0.0.1:8090/api/user"
+const MethodPost = "POST"
+
+var TestUsername = "user_test1"
+var AccessKey = "gyu"
+var SecretKey = "abcdefg"
+
 func NewUser(username string) *User {
 	return &User{
 		Username: username,
